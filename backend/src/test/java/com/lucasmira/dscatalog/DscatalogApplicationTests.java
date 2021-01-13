f0863614fe49522045c6f97fec51b4c352a0a90c
lucas <lucas-mira1@hotmail.com>
2021-01-13 11:23:10 -0300

    Project created

---
+++ backend/src/test/java/com/lucasmira/dscatalog/DscatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lucasmira.dscatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DscatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
