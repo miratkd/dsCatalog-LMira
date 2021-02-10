1e9cae18b70dee0ae6009a50b338a08130eaa8ca
lucas <lucas-mira1@hotmail.com>
2021-02-10 15:47:57 -0300

    Basic bean validation

--- backend/src/main/java/com/lucasmira/dscatalog/dto/UserDTO.java
+++ backend/src/main/java/com/lucasmira/dscatalog/dto/UserDTO.java
@@ -4,14 +4,19 @@ import java.io.Serializable;
 import java.util.HashSet;
 import java.util.Set;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+
 import com.lucasmira.dscatalog.entities.User;
 
 public class UserDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 
 	private Long id;
+	@NotBlank(message = "campo obrigatório")
 	private String firstName;
 	private String lastName;
+	@Email(message = "favor entrar com um email valido")
 	private String email;
 	
 	Set<RoleDTO> roles = new HashSet<>();
