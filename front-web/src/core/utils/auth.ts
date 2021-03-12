bdff3350b3cc8af34e4e07841f41fe492d9c7eaf
lucas <lucas-mira1@hotmail.com>
2021-03-12 17:40:28 -0300

    Add auth background

--- front-web/src/core/utils/auth.ts
+++ front-web/src/core/utils/auth.ts
@@ -32,8 +32,13 @@ export const getSessionData = () => {
 
 export const getAccessTokenDecoded = () => {
     const sessionData = getSessionData();
-    const tokenDecoded = jwtDecode(sessionData.access_token);
-    return tokenDecoded as AccessToken;
+
+    try{
+        const tokenDecoded = jwtDecode(sessionData.access_token);
+        return tokenDecoded as AccessToken;
+    } catch(error){
+        return {} as AccessToken;
+    }
 }
 
 export const isTokenValid = () => {
@@ -57,5 +62,5 @@ export const isAllowedByRole = (routeRoles: Role[] = []) => {
     }
     const {authorities} = getAccessTokenDecoded();
 
-    return routeRoles.some(role => authorities.includes(role));
+    return routeRoles.some(role => authorities?.includes(role));
 }
